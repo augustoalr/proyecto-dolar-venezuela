287346cd4a1268e7ddbf1e425599e0b850aa65bc
augustoalr <augustoalr@gmail.com>
2025-05-04 17:13:05 +0400

    several fixes

--- data.js
+++ data.js
@@ -1,15 +1,5 @@
 
 
-fs.writeFileSync('data.json', JSON.stringify(data, null, 2), 'utf8');
-guardarDataJS(data);
-
-function guardarDataJS(data) {
-  const fileContent = `window.data = ${JSON.stringify(data, null, 2)};`;
-  fs.writeFileSync('data.js', fileContent, 'utf8');
-  console.log('Data written to data.js:', data);
-}
-
-
 window.data = {
   "dolar_oficial": 72.1856,
   "dolar_promedio": 84.7928,
